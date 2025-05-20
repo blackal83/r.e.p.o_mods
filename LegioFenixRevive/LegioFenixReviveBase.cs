a96466d69aa1818ffc98738ee73c5d0770cff2d4
blackal83 <102330714+blackal83@users.noreply.github.com>
2025-05-19 20:37:45 -0600

    Initial Commit

---
+++ LegioFenixRevive/LegioFenixReviveBase.cs
@@ -0,0 +1,47 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+namespace LegioFenixRevive;
+
+[BepInPlugin("legiofenix.blackal.LegioFenixRevive", "LegioFenixRevive", "0.1")]
+public class LegioFenixReviveBase : BaseUnityPlugin
+{
+    private const string pluginGUID = "legiofenix.blackal.LegioFenixRevive";
+
+    private const string pluginName = "LegioFenixRevive";
+
+    private const string pluginVersion = "0.1";
+
+    public static readonly Harmony harmony = new Harmony("LegioFenixRevive");
+
+    internal static LegioFenixReviveBase Instance { get; private set; } = null!;
+    internal new static ManualLogSource Logger => Instance._logger;
+    private ManualLogSource _logger => base.Logger;
+
+    public static ConfigEntry<int> healthToTransfer;
+
+
+
+    private void Awake()
+    {
+        Instance = this;
+        
+        // Prevent the plugin from being deleted
+        this.gameObject.transform.parent = null;
+        this.gameObject.hideFlags = HideFlags.HideAndDontSave;
+
+        LoadConfig();
+        harmony.PatchAll();
+
+        Logger.LogInfo($"{Info.Metadata.GUID} v{Info.Metadata.Version} has loaded!");
+    }
+
+
+    private void LoadConfig()
+    {
+        LegioFenixReviveBase.healthToTransfer = base.Config.Bind<int>("General", "healthToTransfer", 50, "Health that will be moved from the reviver to the revived.  This value + 1 is the minimum health required to revive someone.");
+    }
+}
---
+++ LegioFenixRevive/LegioFenixRevivePatch.cs
@@ -0,0 +1,49 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using LegioFenixRevive;
+using UnityEngine;
+
+namespace LegioFenixRevive;
+
+[HarmonyPatch(typeof(PhysGrabObject), "FixedUpdate")]
+public static class LegioFenixRevivePatch
+{
+    private static void Prefix(ref PhysGrabObject __instance)
+    {
+        if (Input.GetKeyDown((KeyCode)114))
+        {
+            Debug.Log("[Reviver] Holding Key Down.....");
+            if (__instance.name.StartsWith("Player Death Head") && __instance.grabbedLocal)
+            {
+                PlayerDeathHead component = __instance.GetComponent<PlayerDeathHead>();
+                if (component != null)
+                {
+                    PlayerAvatar deadPlayerAvatar = component.playerAvatar;
+                    PlayerHealth deadPlayerHealth = deadPlayerAvatar.playerHealth;
+                    string deadPlayerName = (string)AccessTools.Field(typeof(PlayerAvatar), "playerName").GetValue(deadPlayerAvatar);
+                    //FieldInfo fieldInfo = AccessTools.Field(typeof(PlayerAvatar), "playerName");
+                    //string deadPlayerName = (string)fieldInfo.GetValue(deadPlayerAvatar);
+
+                    PlayerAvatar instance = PlayerAvatar.instance;
+                    PlayerHealth reviverHealth = instance.playerHealth;
+
+                    int reviverHealthValue = (int)AccessTools.Field(typeof(PlayerHealth), "health").GetValue(reviverHealth);
+                    int healthToTransfer = LegioFenixReviveBase.healthToTransfer.Value;
+
+                    if (deadPlayerAvatar != null && reviverHealthValue > healthToTransfer)
+                    {
+                        Debug.Log("[Reviver] Attempting to revive: " + deadPlayerName);
+                        deadPlayerAvatar.Revive(false);
+                        deadPlayerHealth.HealOther(healthToTransfer, true);
+                        reviverHealth.Hurt(healthToTransfer, true, -1);
+                        Debug.Log("[Reviver] Revived: " + deadPlayerName);
+                    }
+                    else
+                    {
+                        Debug.Log("[Reviver] Not able to revive: " + deadPlayerName);
+                    }
+                }
+            }
+        }
+    }
+}
